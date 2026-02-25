da33eefe34131f6d619cd8f92daafc406961ff85
Caua-Andrade <cauaandsilva@gmail.com>
2026-02-25 08:09:20 -0300

    tirei as cores do background de display e teclado

--- src/br/com/cod3r/calc/visao/Display.java
+++ src/br/com/cod3r/calc/visao/Display.java
@@ -1,11 +1,9 @@
 package br.com.cod3r.calc.visao;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class Display extends JPanel {
 
     public Display() {
-        setBackground(Color.GREEN);
     }
 }
--- src/br/com/cod3r/calc/visao/Teclado.java
+++ src/br/com/cod3r/calc/visao/Teclado.java
@@ -1,11 +1,9 @@
 package br.com.cod3r.calc.visao;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class Teclado extends JPanel {
 
     public Teclado() {
-        setBackground(Color.RED);
     }
 }
