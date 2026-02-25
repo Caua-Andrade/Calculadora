3769fd3330dd97f0ee96c4ad4152719b17958b19
Caua-Andrade <cauaandsilva@gmail.com>
2026-02-25 08:21:08 -0300

    alinhei a label e ajustei horizontalmente e verticalmente

--- src/br/com/cod3r/calc/visao/Display.java
+++ src/br/com/cod3r/calc/visao/Display.java
@@ -12,6 +12,9 @@ public class Display extends JPanel {
         label = new JLabel("1234,56");
         label.setForeground(Color.WHITE);
         label.setFont(new Font("courier", Font.PLAIN, 30));
+
+        setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 25));
+
         add(label);
     }
 }
