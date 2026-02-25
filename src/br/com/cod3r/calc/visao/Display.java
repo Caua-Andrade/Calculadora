55f0cbd54a554c309dd1ced26599985344ab1c20
Caua-Andrade <cauaandsilva@gmail.com>
2026-02-25 08:13:00 -0300

    label funcionando

--- src/br/com/cod3r/calc/visao/Display.java
+++ src/br/com/cod3r/calc/visao/Display.java
@@ -4,6 +4,10 @@ import javax.swing.*;
 
 public class Display extends JPanel {
 
+    private final JLabel label;
+
     public Display() {
+        label = new JLabel("1234,56");
+        add(label);
     }
 }
