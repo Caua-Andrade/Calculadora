70aa16713decc85e28473240183f649a59da6b82
Caua-Andrade <cauaandsilva@gmail.com>
2026-02-25 08:05:50 -0300

    ajustei a proporção do display

--- src/br/com/cod3r/calc/visao/Calculadora.java
+++ src/br/com/cod3r/calc/visao/Calculadora.java
@@ -23,6 +23,7 @@ public class Calculadora extends JFrame {
         setLayout(new BorderLayout());
 
         Display display = new Display();
+        display.setPreferredSize(new Dimension(233, 60));
         add(display, BorderLayout.NORTH);
 
         Teclado teclado = new Teclado();
