54d7f3add0f8b2ab17e60d2a696f92f4d6da6c3d
Caua-Andrade <cauaandsilva@gmail.com>
2026-02-26 08:16:41 -0300

    defini as cores

--- src/br/com/cod3r/calc/visao/Teclado.java
+++ src/br/com/cod3r/calc/visao/Teclado.java
@@ -1,9 +1,18 @@
 package br.com.cod3r.calc.visao;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Teclado extends JPanel {
 
+    private final Color COR_CINZA_ESCURO = new Color(68, 68, 68);
+    private final Color COR_CINZA_CLARO = new Color(99, 99, 99);
+    private final Color COR_LARANJA = new Color(242, 163, 60);
+
     public Teclado() {
+
+        setLayout(new GridLayout(5, 4));
+
+        add(new Botao("AC", ))
     }
 }
