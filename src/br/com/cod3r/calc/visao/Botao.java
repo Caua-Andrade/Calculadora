61767d0d162184996288ac62568ebc97ec2a24d6
Caua-Andrade <cauaandsilva@gmail.com>
2026-02-26 08:13:41 -0300

    Criei o construtor do botão

--- src/br/com/cod3r/calc/visao/Botao.java
+++ src/br/com/cod3r/calc/visao/Botao.java
@@ -1,6 +1,14 @@
 package br.com.cod3r.calc.visao;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Botao extends JButton {
+
+    public Botao(String texto, Color cor) {
+        setText(texto);
+        setOpaque(true);
+        setBackground(cor);
+        setFont(new Font("courier", Font.PLAIN, 25));
+    }
 }
