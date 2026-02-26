9ba1234d8afad6fa4bbe751d1781d00506eec9f3
Caua-Andrade <cauaandsilva@gmail.com>
2026-02-26 08:25:59 -0300

    apaguei o teste

--- src/br/com/cod3r/calc/visao/Teclado.java
+++ src/br/com/cod3r/calc/visao/Teclado.java
@@ -11,31 +11,5 @@ public class Teclado extends JPanel {
 
     public Teclado() {
 
-        setLayout(new GridLayout(5, 4));
-
-        add(new Botao("AC", COR_CINZA_ESCURO));
-        add(new Botao("+/-", COR_CINZA_ESCURO));
-        add(new Botao("%", COR_CINZA_ESCURO));
-        add(new Botao("/", COR_LARANJA));
-
-        add(new Botao("7", COR_CINZA_CLARO));
-        add(new Botao("8", COR_CINZA_CLARO));
-        add(new Botao("9", COR_CINZA_CLARO));
-        add(new Botao("*", COR_LARANJA));
-
-        add(new Botao("7", COR_CINZA_CLARO));
-        add(new Botao("8", COR_CINZA_CLARO));
-        add(new Botao("9", COR_CINZA_CLARO));
-        add(new Botao("*", COR_LARANJA));
-
-        add(new Botao("7", COR_CINZA_CLARO));
-        add(new Botao("8", COR_CINZA_CLARO));
-        add(new Botao("9", COR_CINZA_CLARO));
-        add(new Botao("*", COR_LARANJA));
-
-        add(new Botao("7", COR_CINZA_CLARO));
-        add(new Botao("8", COR_CINZA_CLARO));
-        add(new Botao("9", COR_CINZA_CLARO));
-        add(new Botao("*", COR_LARANJA));
     }
 }
